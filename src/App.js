317286c7c07c7ac390841a20dab1f93849737c50
osalvatierra <oscar@owebdesigns.com>
2022-12-18 17:44:30 -0500

    adding Routes, Buttons, JSON Server fake data, CORS Protocol, and styling

--- src/App.js
+++ src/App.js
@@ -1,57 +1,117 @@
 import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Tasks from './components/Tasks';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import TaskDetails from "./components/TaskDetails";
+
 
 function App() {
 const [showAddTask, setShowAddTask] = useState(false)
-const [tasks, setTasks ] = useState(
-  [
-      {
-          id: 1,
-          text: 'Doctors Appointment',
-          day: 'Feb 5th at 2:30pm',
-          reminder: true
-      },
-      {
-          id: 2,
-          text: 'Meeting at School',
-          day: 'Feb 6th at 1:30pm',
-          reminder: true
-      },
-      {
-          id: 3,
-          text: 'Food Shopping',
-          day: 'Feb 5th at 2:30pm',
-          reminder: false
-      }
-  ]
-)
+const [tasks, setTasks ] = useState([])
+
+
+useEffect(()=> {
+  
+  const getTasks = async () => {
+    const tasksFromServer = await fetchTasks();
+    setTasks(tasksFromServer)
+  }
+
+  getTasks();
+}, [])
+
+// Fetch Tasks
+const fetchTasks = async () => {
+  const res = await fetch('/tasks')
+  const data = await res.json();
+
+  return data
+}
+
+// Fetch Task, singular
+const fetchTask = async (id) => {
+  const res = await fetch(`/tasks/${id}`)
+  const data = await res.json();
+
+  return data
+}
 
 //Add Task
-const addTask = (task) => {
-   const id = Math.floor(Math.random() * 10000 ) + 1;
+const addTask = async (task) => {
+
+  const res = await fetch(`/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json();
+
+  setTasks([...tasks, data])
+  //  const id = Math.floor(Math.random() * 10000 ) + 1;
    
-   const newTask = { id, ...task};
-   setTasks([...tasks, newTask])
+  //  const newTask = { id, ...task};
+  //  setTasks([...tasks, newTask])
 }
 
 //Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`/tasks/${id}`, {
+    method: 'DELETE'
+  })
+
   setTasks(tasks.filter((task) => task.id !== id ));
 }
 
 //toggle reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id == id ? { ...task, reminder: !task.reminder} : task))
+const toggleReminder = async (id) => {
+
+  const taskToToggle = await fetchTask(id);
+  const upDTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(upDTask)
+
+  })
+
+  const data = await res.json();
+
+  setTasks(
+    tasks.map((task) => 
+      task.id == id ? { ...task, reminder: data.reminder} : task))
 }
 
   return (
+    <Router>
     <div className="container">
       <Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+      
+      <Routes>
+        <Route 
+          path="/" 
+          element ={
+            <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+            </>
+          }/>
+
+      <Route path='/task/:id' element={<TaskDetails />} />
+      <Route path='/about' element={<About />} />
+
+      </Routes>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import Button from "./Button";
+
+export const About = () => {
+  return (
+    <div>
+        <h4>Version 1.0.0</h4>
+        
+        <a href="/"><Button text='Go Back' /></a>
+    </div>
+  )
+}
+export default About
