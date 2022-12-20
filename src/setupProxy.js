f3d611ce507a924be7ff929f04ec645699a5aa2c
osalvatierra <oscar@owebdesigns.com>
2022-12-20 12:17:35 -0500

    small revisions

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         "/tasks",
         createProxyMiddleware( {
-            target: "http://localhost:5000",
+            target: "https://my-json-server.typicode.com/osalvatierra/tasks/",
             changeOrigin: true,
         })
     );
