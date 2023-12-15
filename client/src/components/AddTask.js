e160de79353d1fc7f19a55cc49b9ad6c8d734bef
osalvatierra <oscar@owebdesigns.com>
2023-12-15 15:33:04 -0500

    updating baseURL for AddTask

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,79 +1,97 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 const client = axios.create({
-  baseURL: "http://localhost:5001" 
+  baseURL: "https://react-task-tracker-server.onrender.com",
 });
 
-const AddTask = ({setTasks, fetchTasks}) => {
-    const [form, setForm] = useState({
-        text: "",
-        day: "",
-        reminder: false,
-      });   
-      
-      function updateForm(value) {
-        return setForm((prev) => {
-          return { ...prev, ...value };
-        });
-      }
+const AddTask = ({ setTasks, fetchTasks }) => {
+  const [form, setForm] = useState({
+    text: "",
+    day: "",
+    reminder: false,
+  });
 
-      function handleClick(event) {
-        event.preventDefault();
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
 
-        //validation
-        if(!form.text || !form.day ) {
-          alert("Please add task");
-          setForm({ text: "", day: "", reminder: false });
-          return;
-        }
+  function handleClick(event) {
+    event.preventDefault();
 
-        const newTask = {
-          text: form.text,
-          day: form.day,
-          reminder: form.reminder,
-        }
-        
-        client.post('/tasks/add', newTask)
-        .then(res => {
-          // Fetch Tasks
-          (async () => {
-            const tasksFromServer = await fetchTasks();
-            setTasks(tasksFromServer);
-          })();
-      
-        }).catch((err) => {
-          console.log(err);
-          return;
-        })
-        setForm({
-          text: "",
-          day: "",
-          reminder: false,
-        })
-      }
+    //validation
+    if (!form.text || !form.day) {
+      alert("Please add task");
+      setForm({ text: "", day: "", reminder: false });
+      return;
+    }
 
+    const newTask = {
+      text: form.text,
+      day: form.day,
+      reminder: form.reminder,
+    };
+
+    client
+      .post("/tasks/add", newTask)
+      .then((res) => {
+        // Fetch Tasks
+        (async () => {
+          const tasksFromServer = await fetchTasks();
+          setTasks(tasksFromServer);
+        })();
+      })
+      .catch((err) => {
+        console.log(err);
+        return;
+      });
+    setForm({
+      text: "",
+      day: "",
+      reminder: false,
+    });
+  }
 
   return (
     <form className="add-form">
-        <div className="form-control">
-            <label>Task</label>
-            <input type='text' placeholder="Add Task" value={form.text} onChange={(e)=> updateForm({ text: e.target.value })}/>
-        </div>
-        <div className="form-control">
-            <label>Day & Time</label>
-            <input type='text' placeholder="Add Day & Time" value={form.day} onChange={(e)=> updateForm({ day: e.target.value })} />
-        </div>
-        <div className="form-control form-control-check">
-            <label>Set Reminder</label>
-            <input type='checkbox' checked={form.reminder} value={form.reminder} onChange={(e)=> updateForm( { reminder: e.currentTarget.checked })} />
-        </div>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={form.text}
+          onChange={(e) => updateForm({ text: e.target.value })}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          placeholder="Add Day & Time"
+          value={form.day}
+          onChange={(e) => updateForm({ day: e.target.value })}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={form.reminder}
+          value={form.reminder}
+          onChange={(e) => updateForm({ reminder: e.currentTarget.checked })}
+        />
+      </div>
 
-        <input type='submit' value='Save Task' className="btn btn-block" onClick={handleClick} />
+      <input
+        type="submit"
+        value="Save Task"
+        className="btn btn-block"
+        onClick={handleClick}
+      />
     </form>
-  )
-}
+  );
+};
 
-export default AddTask
- 
+export default AddTask;
